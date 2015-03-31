a9c4948ec1be55af85e77546a7611984789b39a0
Alex Kuhles <alexak75@gmail.com>
2015-03-31 17:37:39 -0400

    Added onCreateView

--- app/src/main/java/com/ramapo/akuhles/threestones/fragments/BoardFragment.java
+++ app/src/main/java/com/ramapo/akuhles/threestones/fragments/BoardFragment.java
@@ -1,9 +1,13 @@
 package com.ramapo.akuhles.threestones.fragments;
 
 import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.view.Display;
+import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by Alex on 3/29/2015.
@@ -13,6 +17,12 @@ public class BoardFragment extends Fragment implements View.OnTouchListener {
     @Override
     public boolean onTouch(View v, MotionEvent event) {
         return false;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
     }
 
     private void createBoard() {
--- app/src/main/java/com/ramapo/akuhles/threestones/fragments/MenuFragment.java
+++ app/src/main/java/com/ramapo/akuhles/threestones/fragments/MenuFragment.java
@@ -1,10 +1,21 @@
 package com.ramapo.akuhles.threestones.fragments;
 
 import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by Alex on 3/29/2015.
  */
 public class MenuFragment extends Fragment {
 
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+    
 }
